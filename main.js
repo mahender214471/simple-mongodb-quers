5fd1c55c001729cc48db6fc49b61c1eedc58fe21
Mahender rajput <mahender214471@gmail.com>
2022-10-26 11:19:07 +0530

    Git inislize

---
+++ main.js
@@ -0,0 +1,41 @@
+const moment   = require('moment');
+const mpngoose = require('mongoose');
+const GenerateMethods = require('./methods');
+
+//! CLASS FOR MANAGE MONGODB QUERYS
+class momgodb {
+    constructor () {
+         this.schemas = {} ;
+         this.models  = {} ;
+         this.querys  = {} ;
+    }
+    async connect ( URL , options ) {
+          try{
+              await mongoose.connect(URL , options) ;
+              // ! Generate custem methods 
+
+              // ! Print succesfully connection message
+              console.log(`App successfully connect with mongodb : - ${URL}`);
+          }
+          catch(err){
+             console.log(err);
+             throw new Error(err);
+          }
+    }
+    
+    createSchema ( schema ) {
+          const docSchema = new mongoose.Schema({
+             cretedAt:{type:Number , default:moment().valueOf()},
+             updatedAt:{type:Number , default:moment().valueOf()},
+             ...schema,
+             deletedAt:{type:Boolean , default:false}
+          } , { versionKey:false , autoCreate:true , autoIndex:true , timestamps:true})
+          return docSchema ;
+    }
+
+    createModel ( collectionName , collectionSchema ) {
+         const model = mongoose.model( collectionName , collectionSchema ) ;
+         this.models[collectionName] = model ;
+         this.schemas[collectionName] = collectionSchema ;
+    }
+}
